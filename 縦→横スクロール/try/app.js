0fcab6ec4e6c92913fee9f23aceb4dd9ab05a1b3
StockRoomRat <13stck@gmail.com>
2025-08-23 19:38:15 +0900

    #

---
+++ 縦→横スクロール/try/app.js
@@ -0,0 +1,17 @@
+const listWrapperEl = document.querySelector(".side-scroll__wrapper");
+const listEl = document.querySelector(".side-scroll-list");
+
+gsap.to(listEl, {
+    x: () => -(listEl.clientWidth - listWrapperEl.clientWidth),
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".side-scroll",
+        markers: true,
+        start: "top top",
+        end: () => `+=${listEl.clientWidth - listWrapperEl.clientWidth}`,
+        scrub: true,
+        pin: true,
+        anticipatePin: 1,
+        invalidateOnRefresh: true,
+    },
+});
